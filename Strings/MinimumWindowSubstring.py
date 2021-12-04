8489d3d39f76f46593f9715ff052e8df77b15ce1
Shubham <nandcmos@gmail.com>
2021-12-04 12:07:15 +0530

    efficient min window substring

--- Strings/MinimumWindowSubstring.py
+++ Strings/MinimumWindowSubstring.py
@@ -1,50 +1,37 @@
-def minWindow(s,p):
-            allchar = {i for i in p}
-            if(len(s)<len(p)): return ""
-            dubilicate = allchar
-            rd = []
-            mp = [0]*256
-            dt={}
-            for i in p:
-                # if mp[ord(i)] == 0:
-                    mp[ord(i)] += 1
-                    if i not in dt:
-                        dt[i]=1
-                    else : dt[i]+=1
-            ct = 0
-            l,r = 0,0
-            minl,minr = -1,len(s)-1
-            while(r<len(s)):
-                if(mp[ord(s[r])] > 0):
-                    ct += 1
-                    mp[ord(s[r])]-=1
-                rd.append(s[r])
+'''Idea
+Step 1 create a count of all character from pattern and a unique cnt variable for disticnt charaters
+Step 2 read the given string and decrement the count as the character you read
+if total count of character is zero decrement cnt variable signifying that we have read all the 
+characters of pattern in the given string
+if cnt ==0
+measure the length of the string read and store if minimum
+gradually move the left pointer while incrementing the character count in the string(the count mostly is negative
+for the string not in pattern zero for the characters in pattern)
+So if a charater from the left is removed that is in pattern then its count value is 1 therefore 
+we increment cnt variable to say that we require that character and right pointer is incremented till
+that character is found 
+'''
 
-                if(ct == len(p)):
-                    pd = rd[1:]
-                    dpt={}
-                    for ch in pd:
-                        if ch not in dpt:
-                            dpt[ch] = 1
-                        else:
-                            dpt[ch]+=1
-                while(ct == len(p)):
-                    pd = rd[1:]
-                    if s[l] not in allchar or (s[l] in pd and dpt[s[l]] >= dt[s[l]]):
-                        l+=1
-                        dpt[pd[0]]-=1
-                        rd = pd
-                        continue
-                    else:
-                        if r-l < minr-minl:
-                            minl = l
-                            minr = r
-                        mp[ord(s[l])]=1
-                        l+=1
-                        ct-=1
-                        rd = pd
-                r+=1
-            if minl == -1 : return ""
-            return s[minl:minr+1]
+
+def minWindow(s,p):
+    mp= [0]*256
+    for ch in p:
+        mp[ord(ch)]+=1
+    cnt = len({i for i in p})
+    l,j,minlen=-1,0,999999
+    for i,ch in enumerate(s):
+        mp[ord(ch)] -=1
+        if(mp[ord(ch)]==0): cnt-=1
+        while cnt ==0 :
+            ans = i-j+1
+            if ans < minlen:
+                minlen=ans
+                l=j
+            mp[ord(s[j])] +=1
+            if mp[ord(s[j])] == 1 : cnt +=1
+            j+=1
+    if l==-1 : return ""
+    return s[l:l+minlen]
+
 
 print(minWindow("this is a test string","tist"))
