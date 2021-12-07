5f5b5d4a78b3768d77a34e61bbef70cc82a182f2
Shubham <nandcmos@gmail.com>
2021-12-07 09:25:26 +0530

    Bst from Sorted array

---
+++ BST/bstFromSortedArr.py
@@ -0,0 +1,16 @@
+from implementation import BST, inorder
+import implementation as im
+
+def construct(arr):
+    if len(arr) == 0:
+        return
+    mid = int(len(arr)/2)
+    root = BST(arr[mid])
+    root.left = construct(arr[:mid])
+    root.right = construct(arr[mid+1:])
+    return root
+arr = [1,2,5,7,10,13,14,15,22]
+a = construct(arr)
+res=[]
+im.inorder(a,res)
+print(res)
